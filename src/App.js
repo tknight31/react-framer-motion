247f45e470061f88c5f88db9b183efc58ed9b910
Tyler Knight <tknightx31@gmail.com>
2021-02-10 19:22:07 -0500

    Imports. Basic entry transition. Alts on images ✅

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { motion } from "framer-motion";
 import { Card, CardGrid, Container, Header } from "./Elements";
 import "./App.css";
 import Menu from "./Menu";
@@ -9,33 +10,65 @@ import green from "./green.png";
 
 function App() {
   return (
-    <div>
+    <motion.div
+      initial={{
+        opacity: 0,
+        x: -50,
+        y: -50,
+        skew: 7.4,
+      }}
+      animate={{
+        opacity: 1,
+        x: 0,
+        y: 0,
+        skew: 0,
+      }}
+      transition={{
+        duration: 1.3,
+      }}
+    >
       <Header>
         <Menu />
         <h1>Header</h1>
       </Header>
       <Container>
-        <h2>Super Cool</h2>
+        <motion.h2
+          initial={{
+            x: 500,
+            y: -0,
+            skew: -7.4,
+          }}
+          animate={{
+            x: 0,
+            y: 0,
+            skew: 0,
+          }}
+          transition={{
+            duration: 1.3,
+          }}
+        >
+          Super Cool
+        </motion.h2>
         <CardGrid>
           <Card style={{ background: "var(--purp)" }}>
             <h3>Some card</h3>
-            <img src={purp} />
+            <img alt="card" src={purp} />
           </Card>
           <Card style={{ background: "var(--blue)" }}>
             <h3>Some card</h3>
-            <img src={blue} />
+            <img alt="card" src={blue} />
           </Card>
           <Card style={{ background: "var(--black)" }}>
             <h3>Some card</h3>
-            <img src={black} />
+            <img alt="card" src={black} />
           </Card>
           <Card style={{ background: "var(--green)" }}>
             <h3>Some card</h3>
-            <img src={green} />
+            <img alt="card" src={green} />
           </Card>
         </CardGrid>
       </Container>
-    </div>
+    </motion.div>
   );
 }
 
