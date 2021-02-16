6d8335597be2e4e8b9abb668a9458ea6ea205544
Tyler Knight <tknightx31@gmail.com>
2021-02-15 21:55:01 -0500

    Created card dismissal transition

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React, { useState } from "react";
-import { motion, useMotionValue, useTransform } from "framer-motion";
+import {
+  motion,
+  useMotionValue,
+  useTransform,
+  AnimatePresence,
+} from "framer-motion";
 import { Card, CardGrid, Container, Header } from "./Elements";
 import "./App.css";
 import Menu from "./Menu";
@@ -14,10 +19,11 @@ import Nav from "./components/Nav";
 function App() {
   const [value, setValue] = useState(0);
   const [isToggled, setToggle] = useState(false);
+  const [isCardActive, setCardActive] = useState(true);
+
   const [isNavOpen, setNavOpen] = useState(false);
   const x = useMotionValue(0);
-  const opacity = useTransform(x, [-200, 0, 200], [0, 1, 0])
-
+  const opacity = useTransform(x, [-200, 0, 200], [0, 1, 0]);
 
   return (
     <motion.div
@@ -72,22 +78,45 @@ function App() {
             <h3>Some card</h3>
             <img alt="card" src={purp} />
           </Card>
+          <AnimatePresence>
+            {isCardActive && (
+              <motion.div
+                exit={{ height: 0, overflow: "hidden", opacity: 0 }}
+                transition={{
+                  opacity: {
+                    duration: 0,
+                  },
+                }}
+              >
+                <Card
+                  onDragEnd={(e, info) => {
+                    if (Math.abs(info.point.x) > 200) {
+                      setCardActive(false);
+                    }
+                  }}
+                  drag="x"
+                  dragConstraints={{
+                    left: 0,
+                    right: 0,
+                  }}
+                  style={{
+                    x,
+                    opacity: isCardActive ? opacity : 0,
+                    background: "var(--blue)",
+                  }}
+                >
+                  <h3>Some card</h3>
+                  <img alt="card" src={blue} />
+                </Card>
+              </motion.div>
+            )}
+          </AnimatePresence>
           <Card
-            drag="x"
-            dragConstraints={{
-              left: 0,
-              right: 0,
+            onClick={() => {
+              setCardActive(true);
             }}
-            style={{
-              x,
-              opacity,
-              background: "var(--blue)",
-            }}
+            style={{ background: "var(--black)" }}
           >
-            <h3>Some card</h3>
-            <img alt="card" src={blue} />
-          </Card>
-          <Card style={{ background: "var(--black)" }}>
             <h3>Some card</h3>
             <img alt="card" src={black} />
           </Card>
